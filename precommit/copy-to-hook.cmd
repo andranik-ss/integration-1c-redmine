cb22b48544a9f5cf28654137cdcdda46f0cb9263
Andranik Simonyan <andranik.s.s@gmail.com>
2016-07-26 15:37:33 +0300

    add pre-commit tools

---
+++ precommit/copy-to-hook.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+@echo copy current version repo
+
+xcopy .\ibService .\..\.git\hooks\ibService\ /Y /E /F
+xcopy .\scripts .\..\.git\hooks\scripts\ /Y /E /F
+xcopy .\pre-commit .\..\.git\hooks\ /Y /F
+mkdir .\..\..\.git\hooks\v8Reader
+xcopy .\v8Reader\V8Reader.epf .\..\.git\hooks\v8Reader\ /Y /F
+xcopy .\v8files-extractor.os .\..\.git\hooks\ /Y /F
+mkdir .\..\..\.git\hooks\tools
+xcopy .\tools\v8unpack.exe .\..\.git\hooks\tools\ /Y /F
+
+cd .\..\
+git config --local core.quotepath false

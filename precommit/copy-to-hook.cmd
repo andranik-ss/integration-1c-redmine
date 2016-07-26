e6920e939ad83b24e28fa56c243ce70371167939
unknown <asimonyan@UAOD-1C1P.iukr.local>
2016-07-26 17:11:22 +0300

    change hooks

--- precommit/copy-to-hook.cmd
+++ precommit/copy-to-hook.cmd
@@ -5,10 +5,10 @@
 xcopy .\ibService .\..\.git\hooks\ibService\ /Y /E /F
 xcopy .\scripts .\..\.git\hooks\scripts\ /Y /E /F
 xcopy .\pre-commit .\..\.git\hooks\ /Y /F
-mkdir .\..\..\.git\hooks\v8Reader
+mkdir .\..\.git\hooks\v8Reader
 xcopy .\v8Reader\V8Reader.epf .\..\.git\hooks\v8Reader\ /Y /F
 xcopy .\v8files-extractor.os .\..\.git\hooks\ /Y /F
-mkdir .\..\..\.git\hooks\tools
+mkdir .\..\.git\hooks\tools
 xcopy .\tools\v8unpack.exe .\..\.git\hooks\tools\ /Y /F
 
 cd .\..\
